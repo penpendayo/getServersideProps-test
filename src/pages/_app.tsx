e304dd2fc2806f07776504da82af984ca47bdf58
Sakiyama Shingo <yamaa.exception@gmail.com>
2023-05-08 12:03:12 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  }, []);
   return <Component {...pageProps} />
 }
