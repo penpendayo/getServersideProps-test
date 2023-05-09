12215c23dafd3c83d490e01b05f6b405a2c15173
Sakiyama Shingo <yamaa.exception@gmail.com>
2023-05-10 08:27:46 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push("/");
-  }, []);
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
